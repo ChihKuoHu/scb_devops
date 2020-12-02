8d0db2b8e07f23193c4bcf89de7854c69b08af02
chihkuohu <chih.kuo.hu@outlook.com>
2020-12-02 23:37:26 +0800

    change lab

--- lab-installation.sh
+++ lab-installation.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-echo "Enabling EPEL Repository$"
+rm -rf lab
+echo "################ Enabling EPEL Repository  ################"
 sudo dnf config-manager --add-repo=https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo
 sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
 
-echo "Enabling Virtualbox as default Hypervisor ~$"
+echo "################ Enabling Virtualbox as default Hypervisor ~ ################"
 sudo dnf install VirtualBox-6.0 -y
 
-echo "Downloading vagrangt rpm package from internet ~$"
+echo "################ Downloading vagrangt rpm package from internet ~ ################"
 wget https://releases.hashicorp.com/vagrant/2.2.10/vagrant_2.2.10_x86_64.rpm
-echo "Intsalling vagrant package ~$"
+
+echo "################ Intsalling vagrant package ~ ################"
 sudo yum localinstall vagrant_2.2.10_x86_64.rpm -y 
-echo "Intsalling vagrant plugins ~$"
+
+echo "################ Intsalling vagrant plugins ~################"
 wget -O lab.zip "https://onedrive.live.com/download?cid=38AC19F33806C642&resid=38AC19F33806C642%217398&authkey=AKhI3TzsB4hEGT8"
 unzip lab.zip
 rm -rf __MACOSX
@@ -19,8 +22,8 @@ cd lab
 vagrant plugin install vagrant-hosts
 vagrant plugin install vagrant-cachier
 
-echo "Getting ssh key ready and waiting for put it into vagrant@control-node ~$"
-mkdir -p .ssh
+echo "################ Getting ssh key ready and waiting for put it into vagrant@control-node ~ ################"
+mkdir .ssh
 ssh-keygen -b 2048 -t rsa -f .ssh/id_rsa -q -N ""
-rm -rf vagrant_2.2.10_x86_64.rpm
-rm -rf lab.zip
+rm -rf ../vagrant_2.2.10_x86_64.rpm
+rm -rf ../lab.zip
